a53ab08eea70a886e0d09dc9719032f5d464f2fe
wvasalex <vas.alex1609@gmail.com>
2020-07-20 11:38:42 +0300

    checkbox

--- src/app/shared/components/checkbox/checkbox.component.ts
+++ src/app/shared/components/checkbox/checkbox.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'r-checkbox',
+  selector: 's-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent implements OnInit {
+  static guid: number = 1;
+
+  public uid: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.uid = 'checkbox_' + CheckboxComponent.guid++;
   }
 
 }
