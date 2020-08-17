a1ec4f1d01f69c8553003946dd74715cf19fe65e
wvasalex <vas.alex1609@gmail.com>
2020-08-17 17:15:51 +0300

    video

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,9 +1,14 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import { ContactsService } from './contacts.service';
 import { ContentBlock, ContentElement } from '@shared/types';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { BaseBlock } from '@shared/blocks/block.component';
 import { ToasterService } from '@shared/toaster/toaster.service';
 
 @Component({
@@ -12,7 +17,9 @@ import { ToasterService } from '@shared/toaster/toaster.service';
   styleUrls: ['./contacts.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent implements OnInit, AfterViewInit {
+  @ViewChild('map', {static: false}) map: ElementRef;
+
   public block: ContentBlock;
 
   public editor: boolean = false;
@@ -28,6 +35,26 @@ export class ContactsComponent implements OnInit {
     this.contactsService.get().subscribe((blocks: ContentBlock[]) => {
       this.block = blocks[0];
       this.changeDetectorRef.detectChanges();
+    });
+  }
+
+  public ngAfterViewInit(): void {
+    const maps = window['google'].maps;
+    const center = new maps.LatLng(55.801106, 37.6378429);
+    const mapOptions = {
+      zoom: 16,
+      disableDefaultUI: true,
+      scrollwheel: false,
+      center: center,
+      mapTypeId: maps.MapTypeId.ROADMAP
+    };
+
+    const map = new maps.Map(this.map.nativeElement, mapOptions);
+
+    new maps.Marker({
+      position: center,
+      map: map,
+      icon: '/assets/icons/contacts/pin.svg'
     });
   }
 
--- src/app/shared/blocks/journal/article-video/article-video.component.ts
+++ src/app/shared/blocks/journal/article-video/article-video.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef, OnDestroy,
+  ViewChild
+} from '@angular/core';
+import videojs from 'video.js';
 import { BaseBlock } from '@shared/blocks/block.component';
 import { ApiService } from '@shared/services/api/api.service';
 import { ArticleContentElement } from '../../../../journal/journal.model';
@@ -9,23 +17,60 @@ import { ArticleContentElement } from '../../../../journal/journal.model';
   styleUrls: ['./article-video.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ArticleVideoComponent extends BaseBlock {
+export class ArticleVideoComponent extends BaseBlock implements AfterViewInit, OnDestroy {
+  @ViewChild('video', { static: false }) video: ElementRef;
+
+  private player: videojs.Player;
+
   constructor(
     private changeDetectoRef: ChangeDetectorRef,
     private api: ApiService) {
     super();
   }
 
+  public ngAfterViewInit(): void {
+    this.initPlayer();
+  }
+
+  public ngOnDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
   public $upload(e) {
     const file = e.target.files[0];
     const data = new FormData();
     data.append('content_file', file);
-console.log('upload');
+
     this.api.postFile('/api/v1/blog_content_elements/' + this.id + '/', data)
       .toPromise()
       .then((element: ArticleContentElement) => {
         this.contentFile = element.content_file;
         this.changeDetectoRef.detectChanges();
       });
+  }
+
+  private initPlayer() {
+    if (this.player) {
+      this.player.dispose();
+    }
+
+    if (!this.contentFile) {
+      return;
+    }
+
+    this.player = videojs(this.video.nativeElement, {
+      sources: [
+        {
+          type: 'video/mp4',
+          src: this.contentFile,
+        },
+      ],
+      controls: true,
+      autoplay: true,
+    }, () => {
+      console.log('ready!',this.contentFile);
+    });
   }
 }
