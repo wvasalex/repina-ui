1018fb2abc67d52cd62151f29f026d2992323271
Alex <vas.alex1609@gmail.com>
2020-08-26 12:33:13 +0300

    project editor

--- src/app/projects/project/project-block/project-block.component.ts
+++ src/app/projects/project/project-block/project-block.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit } from '@angular/core';
 import { BaseBlock } from '@shared/blocks/block.component';
 import { SelectOption } from '@shared/components/select/select.model';
+import { ContentElement } from '@shared/types';
 
 @Component({
   selector: 'r-project-block',
@@ -12,14 +13,17 @@ export class ProjectBlockComponent extends BaseBlock implements OnInit {
   @Input() availableElements: SelectOption[];
 
   @HostBinding('class.full-width') fullWidth: boolean = false;
-  @HostBinding('class.media') media: boolean = false;
+  @HostBinding('class.has-media') hasMedia: boolean = false;
+  @HostBinding('class.first-media') media: boolean = false;
 
   public ngOnInit() {
     const elements = this.elements;
-    const is_media = (element) => {
+    const is_media = (element: ContentElement) => {
       return element.element_type === 'project-image' ||
         element.element_type === 'project-video';
     }
+
+    this.hasMedia = elements.length > 0 && elements.filter(is_media).length > 0;
 
     if (elements?.length > 1 && is_media(elements[0])) {
       this.media = true;
