d7f557d91c698f9da57229899a88105be41bb38c
Alex <vas.alex1609@gmail.com>
2020-08-26 22:31:34 +0300

    project editor block seletion

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -9,7 +9,7 @@ import { SharedModule } from '@shared/components/shared.module';
 import { GridModule } from '@shared/grid/grid.module';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { ProjectDetailStepComponent } from './project-detail/project-detail-step/project-detail-step.component';
-import { ProjectDetailRolesComponent } from './project-detail/project-detail-roles/project-detail-roles.component';
+import { ProjectDetailRolesComponent } from './project/project-detail-roles/project-detail-roles.component';
 import { ProjectComponent } from './project/project.component';
 import { BlocksModule } from '@shared/blocks/blocks.module';
 import { ProjectRootComponent } from './project/project-root/project-root.component';
