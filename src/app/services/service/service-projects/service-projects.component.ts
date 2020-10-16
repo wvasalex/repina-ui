4138e28619271dc0e5861b16963ddab8281e9e38
Vasilchuk Alex <vas.alex1609@gmail.com>
2020-10-16 14:38:43 +0300

    service related projects filter

--- src/app/services/service/service-projects/service-projects.component.ts
+++ src/app/services/service/service-projects/service-projects.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProjectsService } from '@shared/projects/projects.service';
 import { Project } from '@shared/projects/projects.model';
-import { ServiceScope, ServiceTag } from '../../services.model';
+import { Service, ServiceScope, ServiceTag } from '../../services.model';
 import { BaseBlock } from '@shared/blocks/block.component';
+import { StrMap } from '@shared/types';
 
 @Component({
   selector: 'r-service-projects',
@@ -13,19 +14,27 @@ import { BaseBlock } from '@shared/blocks/block.component';
 })
 export class ServiceProjectsComponent extends BaseBlock {
 
-  @Input() tag: ServiceTag;
-  @Input() scope: ServiceScope;
-
-  public projects$: Observable<Project[]> = this.projectsService.get({
-    per_page: 7,
-  });
+  public projects$: Observable<Project[]>;
 
   constructor(private projectsService: ProjectsService) {
     super();
   }
 
   ngOnInit(): void {
+    const service: Service = this.data.service as Service;
 
+    const query: StrMap<number> = {
+      per_page: 7,
+    };
+
+    if (service.tag) {
+      query.tag_id = (service.tag as ServiceTag).id;
+    }
+    if (service.activity_scope) {
+      query.activity_scope_id = (service.activity_scope as ServiceScope).id;
+    }
+
+    this.projects$ = this.projectsService.get(query);
   }
 
 }
