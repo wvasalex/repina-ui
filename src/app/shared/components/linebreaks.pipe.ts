f1c29a0b1461d0b09219c53e0e0a0291c3adab91
Alex <vas.alex1609@gmail.com>
2020-12-02 19:19:22 +0300

    bug fix

--- src/app/shared/components/linebreaks.pipe.ts
+++ src/app/shared/components/linebreaks.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LinebreaksPipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): unknown {
-    return value.replace(/\n/g, '<br>').replace(/&#8212;/g, '<br>');
+    return value.replace(/\n/g, '<br>').replace(/&#8212;/g, '<br>').replace(/\s([а-яА-Яa-zA-Z]{1,2})\s/gi, ' $1&nbsp;').replace(/\\/g, '<br>');
   }
 
 }
