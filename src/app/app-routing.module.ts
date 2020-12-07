12fe9a4f39d918d702f5c573c0c145113a9c3315
Vasilchuk Alex <vas.alex1609@gmail.com>
2020-12-07 14:51:05 +0300

    404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
-import { AgencyComponent } from './agency/agency.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
