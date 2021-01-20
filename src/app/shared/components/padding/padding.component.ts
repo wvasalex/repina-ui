562e48773d4d9012e9306bbba8142e787f9bdab1
Alex <vas.alex1609@gmail.com>
2021-01-21 02:25:25 +0300

    agency paddings

--- src/app/shared/components/padding/padding.component.ts
+++ src/app/shared/components/padding/padding.component.ts
@@ -7,6 +7,7 @@ import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit } from '
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaddingComponent implements OnInit {
+
   @Input() @HostBinding('class.horizontal') horizontal: boolean = true;
   @Input() @HostBinding('class.disabled') disabled: boolean = false;
 
