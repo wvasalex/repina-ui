6b2a2802385c4a1698dd637c4d3e060f3275bd19
Alex <vas.alex1609@gmail.com>
2021-02-01 16:05:01 +0300

    mobile fix

--- src/app/services/service/service.component.ts
+++ src/app/services/service/service.component.ts
@@ -60,7 +60,6 @@ export class ServiceComponent implements OnInit, OnDestroy {
     private activatedRoute: ActivatedRoute,
     private servicesRenderService: ServicesRenderService,
     private footerService: FooterService,
-    private ref: ElementRef,
   ) {
   }
 
