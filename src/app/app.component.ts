90ec6484b5f0818e35e56e5dce0d9ea5bb87d0a0
Alex <vas.alex1609@gmail.com>
2021-02-24 19:22:05 +0300

    mango on router change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+
+  private _navigationSub: Subscription;
+
+  constructor(private router: Router) {
+    this._navigationSub = this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        if (typeof window !== 'undefined') {
+          (window as any).initMango();
+        }
+      }
+    });
+  }
+
+  public ngOnDestroy() {
+  }
+
 }
